12951aa31f29f0358e9e4726d820062e0d0a6843
Simon Hearne <simon.hearne@gmail.com>
2021-03-02 21:44:56 +0000

    remove snipcart, analytics & maps

--- js/_style-picker.js
+++ js/_style-picker.js
@@ -1,12 +1,19 @@
 (function() {
 	"use strict";
-
-	$(".style-picker").on("click", "> div", function (e) {
-		var $el = $(e.currentTarget),
-			id = $el.data("itemId"),
-			$parent = $el.closest(".styles");
-
-		$parent.children(".style").hide();
-		$parent.children(".style[data-item-id=" + id + "]").show();
-	});
+	let els = document.querySelectorAll(".style-picker > div");
+	for (let el of els) {
+		el.addEventListener("click",e => {
+			let el = e.currentTarget;
+			let id = el.dataset.itemId;
+			let parent = el.parentElement.parentElement;
+			let children = parent.querySelectorAll('.style');
+			for (let child of children) {
+				if (child.dataset.itemId === id) {
+					child.style.display = 'block';
+				} else {
+					child.style.display = 'none';
+				}
+			}
+		})
+	}
 })();
